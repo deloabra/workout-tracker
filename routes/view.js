8cd50bd7a57eb3c43548619bcdc1ccb11400df12
Brayden DeLoach <brayden@deloach.us>
2020-03-11 12:52:46 -0500

    Fixed exercise route bugs and added missing semicolons.

--- routes/view.js
+++ routes/view.js
@@ -7,11 +7,11 @@ module.exports = function(app){
     });
 
     app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/execise.html"));
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
 
     app.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"));
-    })
+    });
 
 };
